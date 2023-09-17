f7f61601b4cd1345d39138b47be4443c64153dac
elfadili-ae <elfadili.ae@gmail.com>
2023-09-17 16:50:06 +0100

    Script: place class updated

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -12,9 +13,8 @@ class City(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        '''places = relationship("Place", backref="cities",
-                              cascade="delete")
-        '''
+        places = relationship("Place", backref="cities", cascade="delete")
+
     else:
         state_id = ""
         name = ""
