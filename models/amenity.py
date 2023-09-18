338ebb8f9dc5b53b0c7e0ee849318d021ff60a53
zrdouane <ramirdouane@gmail.com>
2023-09-18 10:04:32 +0100

    test

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
-from models.base_model import BaseModel, Base
+#!/usr/bin/python3
+""" State Module for the HBNB project """
 from os import getenv
-import sqlalchemy
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
+    """class for amenities"""
+    __tablename__ = 'amenities'
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       back_populates="amenities")
     else:
         name = ""
